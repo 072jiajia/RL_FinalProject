6181f2f8e8f295cbb2dfefc7ca68eb8037139746
Ardian Umam <ardian@informatika.org>
2021-06-28 00:02:22 +0800

    add LunarLander

--- exp_lunarlander.sh
+++ exp_lunarlander.sh
@@ -1,13 +1,10 @@
 ENV='LunarLander-v2'
-NUM_MODEL='1'
-GPU_NO='6'
-N_EPISODES='1000'
-LR='1e-3'
-python AveragedDQN.py --gpu_no=$GPU_NO --seed=0 --num_model=$NUM_MODEL --env_name=$ENV --n_episodes=$N_EPISODES --lr=$LR &
-python AveragedDQN.py --gpu_no=$GPU_NO --seed=1 --num_model=$NUM_MODEL --env_name=$ENV --n_episodes=$N_EPISODES --lr=$LR &
-python AveragedDQN.py --gpu_no=$GPU_NO --seed=2 --num_model=$NUM_MODEL --env_name=$ENV --n_episodes=$N_EPISODES --lr=$LR &
-python AveragedDQN.py --gpu_no=$GPU_NO --seed=3 --num_model=$NUM_MODEL --env_name=$ENV --n_episodes=$N_EPISODES --lr=$LR &
-python AveragedDQN.py --gpu_no=$GPU_NO --seed=4 --num_model=$NUM_MODEL --env_name=$ENV --n_episodes=$N_EPISODES --lr=$LR &
-python AveragedDQN.py --gpu_no=$GPU_NO --seed=5 --num_model=$NUM_MODEL --env_name=$ENV --n_episodes=$N_EPISODES --lr=$LR &
-python AveragedDQN.py --gpu_no=$GPU_NO --seed=6 --num_model=$NUM_MODEL --env_name=$ENV --n_episodes=$N_EPISODES --lr=$LR &
-python AveragedDQN.py --gpu_no=$GPU_NO --seed=7 --num_model=$NUM_MODEL --env_name=$ENV --n_episodes=$N_EPISODES --lr=$LR
+NUM_MODEL='15'
+GPU_NO='4'
+TOTAL_FRAMES='1000000'
+
+python AveragedDQN_lunarLander.py --gpu_no=$GPU_NO --seed=0 --num_model=$NUM_MODEL --env_name=$ENV --total_frames=$TOTAL_FRAMES &
+python AveragedDQN_lunarLander.py --gpu_no=$GPU_NO --seed=1 --num_model=$NUM_MODEL --env_name=$ENV --total_frames=$TOTAL_FRAMES &
+python AveragedDQN_lunarLander.py --gpu_no=$GPU_NO --seed=2 --num_model=$NUM_MODEL --env_name=$ENV --total_frames=$TOTAL_FRAMES &
+python AveragedDQN_lunarLander.py --gpu_no=$GPU_NO --seed=3 --num_model=$NUM_MODEL --env_name=$ENV --total_frames=$TOTAL_FRAMES &
+python AveragedDQN_lunarLander.py --gpu_no=$GPU_NO --seed=4 --num_model=$NUM_MODEL --env_name=$ENV --total_frames=$TOTAL_FRAMES
